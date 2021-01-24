21952ab18a27350f177b4253d74a916a70703a2c
Stefan Disch <disch@informatik.uni-freiburg.de>
2021-01-24 16:20:50 +0100

    completed chapter2

--- chapter2/name.py
+++ chapter2/name.py
@@ -1,3 +1,16 @@
-name = "Ada Lovelace"
+name = "ada Lovelace"
+print(name.title())
 print(name.upper())
 print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}" # Use f-string (format string, since 3.6)
+print(f"Full name: {full_name.title()}")
+
+print("\nStrip strings")
+language=' python '
+print(language)
+print(language.rstrip())
+print(language.lstrip())
+print(language.strip)
